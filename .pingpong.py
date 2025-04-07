ef4ae26418e2d9ab0e97081bca2f525df359a6e3
husvvvsasd <156013253+husvvvsasd@users.noreply.github.com>
2025-04-07 18:25:25 +0300

    v0.2

--- .pingpong.py
+++ .pingpong.py
@@ -20,25 +20,34 @@ class Player(GameSprite):
         self.rel_time = False
         self.start_rel =  None
         self.health = 3
-        
 
     def update_left(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 5:
            self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 695:
+        if keys_pressed[K_s] and self.rect.y < 350:
             self.rect.y += self.speed
 
     def update_right(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 695:
+        if keys_pressed[K_DOWN] and self.rect.y < 350:
             self.rect.y += self.speed
-
-
-pong1 = Player('pong.png',10 ,10,100,100,10)            
-pong2 = Player('pong.png',100 ,100,100,100,10)            
+class Ball(GameSprite):
+    def __init__(self, sprite_image, x_sprite, y_sprite,x_size, y_size, speed):    
+        super().__init__ (sprite_image, x_sprite, y_sprite,x_size,y_size, speed)     
+        self.speed_x = speed
+        self.speed_y = speed  
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.y <= 0 or self.rect.y >= 450:
+            self.speed_y *= -1 
+ 
+pong1 = Player('pong.png',10 ,100,50,150,10)            
+pong2 = Player('pong.png',450 ,100,50,150,10) 
+ball = Ball('ball.png',10 ,10,50,50,3)           
 color1 = (255, 255, 255)
 game = True
 clock=time.Clock()
@@ -47,17 +56,30 @@ window = display.set_mode((500, 500))
 
 window.fill(color1)
 
+font.init()
+font1 = font.Font(None,35)
 
-
-
-
+finish = False
+font_winp1 = font1.render('WIN p1',True,(0,0,0))
+font_winp2 = font1.render('WIN p2',True,(0,0,0))
 while game:
-    window.fill(color1)    
-    pong1.update_left()
-    pong2.update_right()
-    pong1.reset()
-    pong2.reset()
-
+    if ball.rect.x >= 500:
+        window.blit(font_winp1,(250,250))
+        finish = True
+    elif ball.rect.x <= 0:
+        window.blit(font_winp2,(250,250))
+        finish = True
+    if not finish:
+        window.fill(color1)    
+        pong1.update_left()
+        pong2.update_right()
+        pong1.reset()
+        pong2.reset()
+        ball.update()
+        ball.reset()
+        if sprite.collide_rect(ball,pong1) or sprite.collide_rect(ball,pong2):
+            ball.speed_x *= -1
+        
     for e in event.get():
         if e.type == QUIT:
             game = False
